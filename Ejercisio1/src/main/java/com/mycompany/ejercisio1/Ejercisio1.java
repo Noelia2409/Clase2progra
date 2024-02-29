fe029b104ed8aa7206166116b59336c273a098be
Yurith Noelia <yntorop@ufpso.edu.co>
2024-02-29 15:19:19 -0500

    Avance del ejercicio

--- Ejercisio1/src/main/java/com/mycompany/ejercisio1/Ejercisio1.java
+++ Ejercisio1/src/main/java/com/mycompany/ejercisio1/Ejercisio1.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.ejercisio1;
 
-/**
- *
- * @author SCIS2PC17
- */
 public class Ejercisio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character ("homero", 39, 'M', "A la grande le puse Lisa");
+          homero.saySomething();
+        
+          Character bart=new Character ("bart", 10, 'M', "Ayy caramba");
+          bart.saySomething();
+          
+    }
+    
+    
+    static class Character{
+    String name;
+    int age;
+    char gender;
+    String phrase;
+
+        public  Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase= phrase;
+        }
+    
+    void saySomething(){
+        System.out.println(this.phrase);
+        
+    }
     }
 }
