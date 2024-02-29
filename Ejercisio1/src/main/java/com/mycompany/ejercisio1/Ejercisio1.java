b0739d752f820a343fb63ae85e985feafaa59a0d
Yurith Noelia <yntorop@ufpso.edu.co>
2024-02-29 14:34:08 -0500

    creacion del proyecto

---
+++ Ejercisio1/src/main/java/com/mycompany/ejercisio1/Ejercisio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercisio1;
+
+/**
+ *
+ * @author SCIS2PC17
+ */
+public class Ejercisio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
